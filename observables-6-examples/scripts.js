9a8a463eb792ba436172ad2220fc1b0116aa43e3
Umid Almatov <almatov.us@gmail.com>
2017-10-20 12:44:57 +0500

    rxjs - handle ajax calls and merge them with flatMap()

--- observables-6-examples/scripts.js
+++ observables-6-examples/scripts.js
@@ -3,11 +3,30 @@
 const button = document.querySelector('button');
 const output = document.querySelector('output');
 
-const click$ = Rx.Observable.fromEvent(button, 'click');
+Rx.Observable
+  .fromEvent(button, 'click')
+  .flatMap(getAlbums)
+  .map(response => response.response)
+  .subscribe(render, handleError);
 
-click$
-  .bufferWhen(() => click$.delay(400)) // during 400ms
-  .filter(events => events.length >= 3) // 3 or more events
-  .subscribe(() => {
-    output.textContent = Math.random().toString(36).slice(2);
-  });
+function getAlbums() {
+  const userId = Math.round(Math.random() * 10);
+  return Rx.Observable.ajax(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
+}
+
+function handleError(err) {
+  console.error(err);
+}
+
+function render(albums) {
+  output.innerHTML = '';
+  for(let album of albums){
+    const h1 = document.createElement('h1');
+    h1.innerHTML = album.title;
+    output.appendChild(h1);
+
+    const p = document.createElement('p');
+    p.innerHTML = `id: ${album.id} userId: ${album.userId}`;
+    output.appendChild(p);
+  }
+}
