29eef8f14bdccd9a705045b259b72dc59a621f7b
Umid Almatov <almatov.us@gmail.com>
2017-10-20 22:31:23 +0500

    C basics - avoiding deeply nested 'if' statements with 'else if'

---
+++ c/example_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  int grade;
+
+  puts("Enter your grade:");
+  scanf("%d", &grade);
+
+  if (grade >= 90) {
+    puts("A");
+  }
+  else if (grade >= 80) {
+    puts("B");
+  }
+  else if (grade >= 70) {
+    puts("C");
+  }
+  else if (grade >= 60) {
+    puts("D");
+  }
+  else {
+    puts("F");
+  }
+}
