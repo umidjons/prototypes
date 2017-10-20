3396aceabe54a0d226229a5d011c35c8bf3445e2
Umid Almatov <almatov.us@gmail.com>
2017-10-20 11:54:40 +0500

    rxjs - generate string every 3rd click using observable

--- observables-6-examples/scripts.js
+++ observables-6-examples/scripts.js
@@ -5,6 +5,7 @@ const output = document.querySelector('output');
 
 Rx.Observable
   .fromEvent(button, 'click')
+  .bufferCount(3)
   .subscribe(() => {
     output.textContent = Math.random().toString(36).slice(2);
   });
