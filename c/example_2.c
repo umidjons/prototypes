a7fa0095bf122b233638be34e8f0cd95394c19d5
Umid Almatov <almatov.us@gmail.com>
2017-10-20 20:28:34 +0500

    C basics - printf() and scanf() functions.

---
+++ c/example_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int integer1;
+  int integer2;
+
+  printf("Enter first number\n");
+  scanf("%d", &integer1);
+
+  printf("Enter second number\n");
+  scanf("%d", &integer2);
+
+  int sum = integer1 + integer2;
+
+  printf("Sum is %d\n", sum);
+}
