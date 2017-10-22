bf06afb46d3dbcb671c1dbf9406d3e197ea14951
Umid Almatov <almatov.us@gmail.com>
2017-10-23 00:16:34 +0500

    Added text to read. Implemented adding recordings into list, playing and removing specific items. Page redesigned.

--- sound-recorder/assets/js/scripts.js
+++ sound-recorder/assets/js/scripts.js
@@ -2,12 +2,11 @@ const Rec = {
 
   init() {
     this.state = false;
-    this.playing = false;
     this.files = [];
 
     this.ui = {
       btnToggle: document.getElementById('btn-toggle'),
-      btnPlay: document.getElementById('btn-play'),
+      btnUpload: document.getElementById('btn-upload'),
       audioList: document.getElementById('audio-list')
     };
 
@@ -39,7 +38,7 @@ const Rec = {
     if (this.mic.enabled) {
       this.recorder.record(this.soundFile);
       this.state = true;
-      this.ui.btnToggle.innerHTML = '<i class="fa fa-fw fa-stop"></i> Stop';
+      this.ui.btnToggle.innerHTML = this.icon('microphone-slash', 'Stop');
     } else {
       console.log('Mic is not enabled.');
     }
@@ -48,32 +47,82 @@ const Rec = {
   stop() {
     this.recorder.stop();
     this.state = false;
-    this.ui.btnToggle.innerHTML = '<i class="fa fa-fw fa-microphone"></i> Start';
+    this.ui.btnToggle.innerHTML = this.icon('microphone', 'Start');
     this.addFile();
   },
 
-  play() {
-    if (!this.playing) {
-      this.soundFile.onended(() => {
-        this.playing = false;
-        this.ui.btnPlay.innerHTML = '<i class="fa fa-fw fa-play"></i> Play';
-        console.log('Finished');
-      });
-
-      this.soundFile.play();
-      this.playing = true;
-      this.ui.btnPlay.innerHTML = '<i class="fa fa-fw fa-stop"></i> Stop';
-    } else {
-      this.soundFile.stop();
-    }
+  upload() {
+    // TODO: implement uploading
   },
 
   addFile() {
     this.files.push(this.soundFile);
 
+    let play = (fileName, btn) => {
+      return () => {
+        // find soundFile by fileName
+        let soundFiles = this.files.filter(soundFile => soundFile.fileName === fileName);
+        let soundFile = soundFiles[0];
+
+        if (!soundFile.playing) {
+          soundFile.onended(() => {
+            btn.innerHTML = this.icon('play');
+          });
+
+          btn.innerHTML = this.icon('stop');
+          soundFile.play();
+          soundFile.playing = true;
+        } else {
+          soundFile.stop();
+          soundFile.playing = false;
+        }
+      };
+    };
+
+    let remove = (fileName) => {
+      return () => {
+        if (confirm(`Do you want to delete ${fileName}?`)) {
+          // remove soundFile from files array
+          for (let idx=0; idx < this.files.length; idx++) {
+            if (this.files[idx].fileName === fileName) {
+              this.files.splice(idx, 1);
+              break;
+            }
+          }
+
+          // remove appropriate list item
+          document.querySelector(`#audio-list>li[data-file-name="${fileName}"]`).remove();
+        }
+      };
+    };
+
+    let btnPlay = document.createElement('button');
+    btnPlay.className = 'btn-li btn-play';
+    btnPlay.innerHTML = this.icon('play');
+    btnPlay.addEventListener('click', play(this.soundFile.fileName, btnPlay));
+
+    let btnRemove = document.createElement('button');
+    btnRemove.className = 'btn-li btn-remove';
+    btnRemove.innerHTML = this.icon('times');
+    btnRemove.addEventListener('click', remove(this.soundFile.fileName));
+
+    let text = document.createTextNode(` ${this.soundFile.fileName}`);
+
     let item = document.createElement('li');
-    item.innerHTML = this.soundFile.fileName;
+    item.appendChild(btnPlay);
+    item.appendChild(btnRemove);
+    item.appendChild(text);
+    item.dataset.fileName = this.soundFile.fileName;
+    
     this.ui.audioList.appendChild(item);
+  },
+
+  icon(name, title) {
+    let icon = `<i class="fa fa-fw fa-${name}"></i>`;
+    if (title) {
+      icon += ` ${title}`;
+    }
+    return icon;
   }
 };
 
