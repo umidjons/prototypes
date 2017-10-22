a7084f82af8340df463c19fa85e0bf213f656d78
Umid Almatov <almatov.us@gmail.com>
2017-10-22 10:15:17 +0500

    Recorder improved.

--- sound-recorder/scripts.js
+++ sound-recorder/scripts.js
@@ -1,36 +1,68 @@
-var mic, recorder, soundFile;
+const Rec = {
 
-function setup() {
-  // Connect to the mic
-  mic = new p5.AudioIn();
-  mic.start();
+  init() {
+    this.state = false;
+    this.playing = false;
 
-  // Create a sound recorder
-  recorder = new p5.SoundRecorder();
-  recorder.setInput(mic);
+    this.ui = {
+      btnToggle: document.getElementById('btn-toggle'),
+      btnPlay: document.getElementById('btn-play')
+    };
 
-  // Initialize sound file
-  soundFile = new p5.SoundFile();
-}
+    this.initRecorder();
+  },
 
-function start() {
-  if (mic.enabled) {
-    recorder.record(soundFile);
-    console.log('Recording');
-  } else {
-    console.log('Microphone is not enabled.');
+  toggle() {
+    if (!this.state) {
+      this.start();
+    } else {
+      this.stop();
+    }
+  },
+
+  initRecorder() {
+    this.mic = new p5.AudioIn();
+    this.mic.start();
+
+    this.recorder = new p5.SoundRecorder();
+    this.recorder.setInput(this.mic);
+
+    this.soundFile = new p5.SoundFile();
+  },
+
+  start() {
+    this.soundFile = new p5.SoundFile();
+
+    if (this.mic.enabled) {
+      this.recorder.record(this.soundFile);
+      this.state = true;
+      this.ui.btnToggle.textContent = 'Stop';
+    } else {
+      console.log('Mic is not enabled.');
+    }
+  },
+
+  stop() {
+    this.recorder.stop();
+    this.state = false;
+    this.ui.btnToggle.textContent = 'Start';
+  },
+
+  play() {
+    if (!this.playing) {
+      this.soundFile.onended(() => {
+        this.playing = false;
+        this.ui.btnPlay.textContent = 'Play';
+        console.log('Finished');
+      });
+
+      this.soundFile.play();
+      this.playing = true;
+      this.ui.btnPlay.textContent = 'Abort';
+    } else {
+      this.soundFile.stop();
+    }
   }
-}
+};
 
-function stop() {
-  recorder.stop();
-  console.log('Stopped.');
-}
-
-function play() {
-  soundFile.onended(() => {
-    console.log('Finished.');
-  });
-  soundFile.play();
-  console.log('Playing...');
-}
+Rec.init();
